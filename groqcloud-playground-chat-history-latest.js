c3ed329b136f945a464ff8bd2d890ae82c1b9b3d
hlo-world <9633055+hlo-world@users.noreply.github.com>
2024-08-22 03:28:23 -0400

    Updating latest to version 0.2.0

--- groqcloud-playground-chat-history-latest.js
+++ groqcloud-playground-chat-history-latest.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        Groqcloud Playground Chat History
-// @version     0.1.0
+// @version     0.2.0
 // @description Adds a Chat History on the sidebar of Groqcloud Playground with locally-stored deletable per-session entries
 // @author      Harris Lo
 // @website     https://github.com/hlo-world/groqcloud-playground-chat-history
@@ -107,65 +107,104 @@ $(document).ready(function() {
         }
     `);
 
-    var textSeparation = '<br><hr style="width: 100%; border-color: var(--foreground); border-style: solid;">';
-
     // Function to append textarea content to sidebar when submit request is made
     function addSubmitRequestListener() {
-        $('.h-full.overflow-auto').on('keydown', 'textarea.border-input', function(event) {
+        var userInputTextArea = 'textarea.border-input';
+        var outputGroups = '.h-full.overflow-auto .group';
+        var outputGroupTextArea = 'textarea';
+        var userGroupIdentifier = 'button:contains("user")';
+        var assistantGroupIdentifier = 'button:contains("assistant")';
+        var replyCompletionIdentifier = 'a[data-state="closed"][class*="text-muted-accent"]';
+        var submitButtonIdentifier = 'button:contains("Submit")';
+        var textSeparation = '<br><hr style="width: 100%; border-color: var(--foreground); border-style: solid;">';
+
+        $(userInputTextArea).on('keydown', function(event) {
             if ((event.ctrlKey || event.metaKey) && event.key === 'Enter') {
                 submitHandler();
             }
         });
 
-        $('button:contains("Submit")').click(submitHandler);
+        $(submitButtonIdentifier).click(submitHandler);
 
-        function submitHandler() {
-            function checkGroupsLength() {
-                var userGroups = $('.h-full.overflow-auto .group').filter(function() {
-                    return $(this).find('button:contains("user")').length > 0;
+        async function submitHandler() {
+            function waitForNewGroupsLengthEqual(previousUserGroupsLength) {
+                var userGroups = $(outputGroups).filter(function() {
+                    return $(this).find(userGroupIdentifier).length > 0;
                 });
-                var assistantGroups = $('.h-full.overflow-auto .group').filter(function() {
-                    return $(this).find('button:contains("assistant")').length > 0;
+                var assistantGroups = $(outputGroups).filter(function() {
+                    return $(this).find(assistantGroupIdentifier).length > 0;
                 });
-                if (userGroups.length !== assistantGroups.length) {
-                    setTimeout(checkGroupsLength, 100);
-                    return;
+                if (userGroups.length <= previousUserGroupsLength || userGroups.length !== assistantGroups.length) {
+                    return new Promise((resolve, reject) => {
+                        setTimeout(() => {
+                            resolve(waitForNewGroupsLengthEqual(previousUserGroupsLength));
+                        }, 100);
+                    });
+                } else {
+                    return Promise.resolve({ userGroups, assistantGroups });
                 }
+            }
+            function waitForAssitantReplyCompleted(assistantGroups) {
                 var lastAssistantObject = assistantGroups[assistantGroups.length - 1];
-                // if "●" is shown then completion is not done yet
-                if (lastAssistantObject.textContent.includes("●")) {
-                    setTimeout(checkGroupsLength, 10);
-                    return;
+                const completionElement = document.querySelector(replyCompletionIdentifier);
+                if (completionElement) {
+                    return Promise.resolve();
+                } else {
+                    return new Promise((resolve, reject) => {
+                        setTimeout(() => {
+                            resolve(waitForAssitantReplyCompleted(assistantGroups));
+                        }, 10);
+                    });
                 }
-
+            }
+            function collectNarrative(userGroups, assistantGroups) {
                 var completeNarrative = [];
-
                 for (var i = 0; i < userGroups.length; i++) {
-                    var userText = userGroups.eq(i).find('textarea').text().replace(/\n/g, '<br>');
-                    var assistantText = assistantGroups.eq(i).find('textarea').text().replace(/\n/g, '<br>');
+                    var userText = userGroups.eq(i).find(outputGroupTextArea).text().replace(/\n/g, '<br>');
+                    var assistantText = assistantGroups.eq(i).find(outputGroupTextArea).text().replace(/\n/g, '<br>');
                     var interleavedText = (userText || '') + textSeparation + (assistantText || '');
                     if (!completeNarrative.includes(interleavedText)) {
                         completeNarrative.push(interleavedText);
                     }
                 }
-
+                return completeNarrative;
+            }
+            function appendToSideBar(completeNarrative) {
                 var existingEntry = $('#groqChatHistorySideBar ul li').filter(function() {
                     return $(this).text().includes(formattedDate);
                 });
-
                 if (existingEntry.length > 0) {
                     existingEntry.remove();
                 }
-
                 var flattenedCompleteNarrative = completeNarrative.join(textSeparation);
                 $('#groqChatHistorySideBar ul').prepend(createHistoryEntry(formattedDate, flattenedCompleteNarrative));
                 GM_setValue(keyPrefix + formattedDate, flattenedCompleteNarrative);
                 addDeleteHistoryListener();
             }
 
-            checkGroupsLength();
+            var previousUserGroups = $(outputGroups).filter(function() {
+                    return $(this).find(userGroupIdentifier).length > 0;
+                });
+            var { userGroups, assistantGroups } = await waitForNewGroupsLengthEqual(previousUserGroups.length);
+            await waitForAssitantReplyCompleted(assistantGroups);
+            var completeNarrative = collectNarrative(userGroups, assistantGroups);
+            appendToSideBar(completeNarrative);
         }
     }
 
+    /*
+     * Function to switch playground mode
+     * As of 2024-07-18:
+     *     [0] is Studio mode
+     *     [1] is Chat mode
+    */
+    function switchPlaygroundMode(mode) {
+        // Get the list of mode button elements
+        var modeButtonList = document.querySelectorAll('button.block.rounded-full.text-xs.px-3.py-1.h-auto');
+        // Click the mode button at desired index
+        modeButtonList[mode].click();
+    }
+
     addSubmitRequestListener();
+    switchPlaygroundMode(1);
 });
